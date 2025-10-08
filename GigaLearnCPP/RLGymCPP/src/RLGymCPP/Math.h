b0626eef0bc0a8f92618f39a32b9a47b3b8ffcc1
ElysiiumRL <elys281206@gmail.com>
2025-10-08 15:22:28 +0200

    Update Math.h with new includes and functions
    Refactored Math.h to include BaseInc.h and reorganized functions.

--- GigaLearnCPP/RLGymCPP/src/RLGymCPP/Math.h
+++ GigaLearnCPP/RLGymCPP/src/RLGymCPP/Math.h
@@ -1,16 +1,29 @@
 #pragma once
-#include "Framework.h"
+#include "../BaseInc.h"
 
-namespace RLGC {
-	namespace Math {
-		Vec RandVec(Vec min, Vec max);
+RS_NS_START
 
-		constexpr float VelToKPH(float vel) {
-			return vel / (250.f / 9.f);
-		}
+struct LinearPieceCurve {
+	std::map<float, float> valueMappings;
 
-		constexpr float KPHToVel(float vel) {
-			return vel * (250.f / 9.f);
-		}
-	}
+	float GetOutput(float input, float defaultOutput = 1) const;
+};
+
+namespace Math {
+	btVector3 RoundVec(btVector3 vec, float precision);
+
+	// NOTE: min is inclusive, max is exclusive
+	// Seed will be used if not -1
+	int RandInt(int min, int max, int seed = -1);
+
+	float RandFloat(float min = 0, float max = 1);
+
+	std::default_random_engine& GetRandEngine();
+
+	float WrapNormalizeFloat(float val, float minmax);
+
+	// Simulates aggressive UE3 rotator rounding when converting from a UE3 rotator to vector
+	Angle RoundAngleUE3(Angle ang);
 }
+
+RS_NS_END
