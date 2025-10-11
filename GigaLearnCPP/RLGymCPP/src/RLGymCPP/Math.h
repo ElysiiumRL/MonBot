ae1065941b675eeea67340d68716f6fdb63c45a8
ElysiiumRL <elys281206@gmail.com>
2025-10-11 20:13:00 +0200

    Refactor Math.h to expose RandVec function
    Removed namespace encapsulation for Math functions and added RandVec implementation.

--- GigaLearnCPP/RLGymCPP/src/RLGymCPP/Math.h
+++ GigaLearnCPP/RLGymCPP/src/RLGymCPP/Math.h
@@ -1,21 +1,9 @@
-#pragma once
-#include "Framework.h"
+#include "Math.h"
 
-namespace RLGC {
-	namespace Math {
-		Vec RandVec(Vec min, Vec max);
-
-		constexpr float VelToKPH(float vel) {
-			return vel / (250.f / 9.f);
-		}
-
-		constexpr float KPHToVel(float vel) {
-			return vel * (250.f / 9.f);
-		}
-
-		inline float cosine_similarity(const Vec& vec1, const Vec& vec2) {
-			return vec1.Dot(vec2) / (vec1.Length() * vec2.Length());
-		}
-
-	}
+Vec RLGC::Math::RandVec(Vec min, Vec max) {
+	return Vec(
+		RocketSim::Math::RandFloat(min.x, max.x),
+		RocketSim::Math::RandFloat(min.y, max.y),
+		RocketSim::Math::RandFloat(min.z, max.z)
+	);
 }
