c7ab9c5721cb72d0e1650a69d2ce4309bccbd851
ElysiiumRL <elys281206@gmail.com>
2025-10-13 18:23:31 +0200

    Update Math.h

--- GigaLearnCPP/RLGymCPP/src/RLGymCPP/Math.h
+++ GigaLearnCPP/RLGymCPP/src/RLGymCPP/Math.h
@@ -12,5 +12,10 @@ namespace RLGC {
 		constexpr float KPHToVel(float vel) {
 			return vel * (250.f / 9.f);
 		}
+
+		inline float cosine_similarity(const Vec& vec1, const Vec& vec2) {
+			return vec1.Dot(vec2) / (vec1.Length() * vec2.Length());
+		}
+
 	}
 }
