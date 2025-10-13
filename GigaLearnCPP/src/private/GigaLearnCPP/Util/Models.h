d0a8efbaada7681a17fa52cf4cdc43358ab7b135
ElysiiumRL <elys281206@gmail.com>
2025-10-13 18:21:37 +0200

    Refactor Models.h for nullptr usage and iterator improvements

--- GigaLearnCPP/src/private/GigaLearnCPP/Util/Models.h
+++ GigaLearnCPP/src/private/GigaLearnCPP/Util/Models.h
@@ -16,6 +16,11 @@
 #include <GigaLearnCPP/PPO/PPOLearnerConfig.h>
 #include <GigaLearnCPP/Util/ModelConfig.h>
 
+#include <map>
+#include <string>
+#include <filesystem>
+#include <cctype>
+
 namespace GGL {
 
 	inline void AddActivationFunc(torch::nn::Sequential& seq, ModelActivationType type) {
@@ -52,7 +57,7 @@ namespace GGL {
 		}
 
 		RG_ERR_CLOSE("Unknown optimizer type: " << (int)type);
-		return NULL;
+		return nullptr;
 	}
 
 	inline void SetOptimizerLR(torch::optim::Optimizer* optimizer, ModelOptimType type, float lr) {
@@ -91,7 +96,7 @@ namespace GGL {
 
 		torch::optim::Optimizer* optim;
 
-		Model() : config(PartialModelConfig{}), device({}), modelName(NULL) {} // Uninitialized init
+		Model() : config(PartialModelConfig{}), device({}), modelName(nullptr) {} // Uninitialized init
 
 		Model(
 			const char* modelName,
@@ -100,7 +105,7 @@ namespace GGL {
 		);
 
 		virtual torch::Tensor Forward(torch::Tensor input, bool halfPrec);
-		
+
 		void SetOptimLR(float newLR);
 
 		void StepOptim();
@@ -160,12 +165,12 @@ namespace GGL {
 	public:
 		std::map<std::string, Model*> map = {};
 
-		Model* operator[](const std::string& name) { 
+		Model* operator[](const std::string& name) {
 			auto itr = map.find(name);
 			if (itr == map.end()) {
-				return NULL;
+				return nullptr;
 			} else {
-				return map[name];
+				return itr->second;
 			}
 		};
 
@@ -190,9 +195,15 @@ namespace GGL {
 				model->Load(folder, allowNotExist, loadOptims);
 		}
 
-		class ModelIterator : public std::iterator<std::forward_iterator_tag, typename Model*> {
+		class ModelIterator {
 		public:
+			using iterator_category = std::forward_iterator_tag;
+			using value_type = Model*;
+			using difference_type = std::ptrdiff_t;
+			using pointer = Model**;
+			using reference = Model*&;
 			using MapItr = std::map<std::string, Model*>::iterator;
+
 			MapItr _mapItr;
 
 			ModelIterator(MapItr mapItr) : _mapItr(mapItr) {}
@@ -202,15 +213,16 @@ namespace GGL {
 			bool operator==(const ModelIterator& other) const { return _mapItr == other._mapItr; }
 			bool operator!=(const ModelIterator& other) const { return _mapItr != other._mapItr; }
 
-			typename Model*& operator*() const { return _mapItr->second; }
+			reference operator*() const { return _mapItr->second; }
+			pointer operator->() const { return &(_mapItr->second); }
 		};
 
 		ModelIterator begin() {
-			return map.begin();
+			return ModelIterator(map.begin());
 		}
 
 		ModelIterator end() {
-			return map.end();
+			return ModelIterator(map.end());
 		}
 
 		ModelSet CloneAll() {
